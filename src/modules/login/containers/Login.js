af9e566fa57410ce2985ffcf0030db4b4ac6f72a
gwilym <gwilym@gwilymh.com>
2016-01-11 16:26:57 +1100

    cleanup

--- src/modules/login/containers/Login.js
+++ src/modules/login/containers/Login.js
@@ -1,8 +1,7 @@
 import _ from 'lodash'
 import React from 'react-native'
 import {connect} from 'react-redux/native'
-import {bindActionCreators} from 'redux'
-import {actions as auth_actions} from 'fl-auth-redux'
+import {login} from 'fl-auth-redux'
 
 import Login from '../components/Login'
 import router from '../../../routing'
@@ -15,25 +14,25 @@ class LoginContainer extends React.Component {
 
   onLogin = data => {
     console.log('dispatching', auth_actions.login, data)
-    this.props.dispatch(auth_actions.login(`${this.props.config.get('url')}/login`, data.email, data.password, (err) => {
+    this.props.login(`${this.props.config.get('url')}/login`, data.email, data.password, (err) => {
       console.log('req done', err)
       if (!err) this.props.nav.push(router.get('landing'))
-    }))
+    })
   };
 
   render() {
-    const {auth, dispatch} = this.props
+    const {auth} = this.props
     return (
-      <Login onSubmit={this.onLogin} auth={auth} {...bindActionCreators(auth_actions, dispatch)} />
+      <Login onSubmit={this.onLogin} auth={auth} />
     )
   }
 }
 
 LoginContainer.propTypes = {
   auth: React.PropTypes.object,
-  dispatch: React.PropTypes.func,
   config: React.PropTypes.object.isRequired,
   nav: React.PropTypes.object.isRequired,
+  login: React.PropTypes.func.isRequired,
 }
 
-export default connect(state => _.pick(state, 'auth', 'config'))(LoginContainer)
+export default connect(state => _.pick(state, 'auth', 'config'), {login})(LoginContainer)
